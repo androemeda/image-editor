5e348812ac6ca0b96d5477345e2dfe4984030023
androemeda <kartikdeshpande2810@gmail.com>
2023-08-29 12:55:48 +0530

    third commit

--- imageEditor.java
+++ imageEditor.java
@@ -9,6 +9,137 @@ import java.util.*;
 
 public class imageEditor{
 
+    static BufferedImage inversion(BufferedImage inputImage){
+
+        int height = inputImage.getHeight();
+        int width = inputImage.getWidth();
+
+        BufferedImage outputImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+
+        for(int i=0 ; i<height ; i++){
+
+            for(int j=0 ; j<width ; j++){
+
+                Color pixel = new Color(inputImage.getRGB(j,i));
+
+                int red = pixel.getRed();
+                int green = pixel.getGreen();
+                int blue = pixel.getBlue();
+
+                red = 255-red;
+                green = 255-green;
+                blue = 255-blue;
+
+                Color newPixel = new Color(red , green , blue);
+
+                outputImage.setRGB(j , i , newPixel.getRGB());
+
+            }
+        }
+        return outputImage;
+    }
+
+    static BufferedImage edgeDetection(BufferedImage inputImage){
+
+        int height = inputImage.getHeight();
+        int width = inputImage.getWidth();
+
+        BufferedImage outputImage = new BufferedImage(width+5 , height+5 ,BufferedImage.TYPE_INT_RGB);
+
+        for(int i=0 ; i<height ; i++){
+            for(int j=0 ; j<width ; j++){
+
+                Color pixel = new Color(inputImage.getRGB(j,i));
+
+                outputImage.setRGB(j , i , pixel.getRGB());
+            }
+        }
+
+            for(int i=5 ; i<height+5 ; i++){
+                for(int j=5 ; j<width+5 ; j++){
+
+                    Color pixel = new Color(inputImage.getRGB(j-5 , i-5));
+
+                    Color newPixel = new Color(outputImage.getRGB(j, i));
+
+                    int finalRed = newPixel.getRed()-pixel.getRed();
+                    int finalGreen = newPixel.getGreen()-pixel.getGreen();
+                    int finalBlue = newPixel.getBlue()-pixel.getBlue();
+
+                    if(finalRed<0) {finalRed=0;}
+                    if(finalGreen<0) {finalGreen=0;}
+                    if(finalBlue<0) {finalBlue=0;}
+
+                    Color finalPixel = new Color(finalRed , finalGreen , finalBlue);
+
+                    outputImage.setRGB(j , i , finalPixel.getRGB() );
+                }
+            }
+        return outputImage;
+    }
+
+    static BufferedImage cropToCircle(BufferedImage inputImage , int radius){
+
+        int height = inputImage.getHeight();
+        int width = inputImage.getWidth();
+
+        int centreRow = height/2;
+        int centreColumn = width/2;
+
+        BufferedImage outputImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+
+        Color blackColor = new Color(0 ,0 ,0);
+        for(int i=0 ; i<height ; i++){
+            for(int j=0 ; j<width ; j++){
+                if((j-centreColumn)*(j-centreColumn)+(i-centreRow)*(i-centreRow) > radius*radius){
+                    outputImage.setRGB(j, i , blackColor.getRGB());
+                }else{
+                    outputImage.setRGB(j , i , inputImage.getRGB(j,i));
+                }
+            }
+        }
+        return outputImage;
+    }
+
+    static BufferedImage contrast(BufferedImage inputImage , int percentage){
+
+        int height = inputImage.getHeight();
+        int width = inputImage.getWidth();
+
+        BufferedImage outputImage = new BufferedImage(width , height , BufferedImage.TYPE_INT_RGB);
+
+        for(int i=0 ; i<height ; i++){
+
+            for(int j=0 ; j<width ; j++){
+
+                Color pixel = new Color(inputImage.getRGB(j , i));
+
+                int red = pixel.getRed();
+                int green = pixel.getGreen();
+                int blue = pixel.getBlue();
+
+                if(red>127) {red = red + (red*percentage/100);}
+                else {red = red - (red*percentage/100);}
+                if(green>127) {green = green + (green*percentage/100);}
+                else {green = green - (green*percentage/100);}
+                if(blue>127) {blue = blue + (blue*percentage/100);}
+                else {blue = blue - (blue*percentage/100);}
+
+                if(red>255) {red=255;}
+                if(red<0) {red=0;}
+                if(green>255) {green=255;}
+                if(green<0) {green=0;}
+                if(blue>255) {blue=255;}
+                if(blue<0) {blue=0;}
+
+                Color newPixel = new Color(red , green , blue);
+
+                outputImage.setRGB(j ,i , newPixel.getRGB());
+            }
+        }
+        return outputImage;
+    }
+
     static BufferedImage blurr(BufferedImage inputImage , int pixelCount){
 
         int height = inputImage.getHeight();
@@ -147,16 +278,29 @@ public class imageEditor{
 
 
     static BufferedImage convertToGreyScale(BufferedImage inputImage){
+
         int height = inputImage.getHeight();
         int width = inputImage.getWidth();
 
         BufferedImage outputImage = new BufferedImage(width , height , BufferedImage.TYPE_BYTE_GRAY);
+
         for(int i=0 ; i<height ; i++){
+
             for(int j=0 ; j<width ; j++){
+
                 outputImage.setRGB(j,i, inputImage.getRGB(j,i));
 
             }
         }
+        return outputImage;
+    }
+
+    static BufferedImage pencilSketch(BufferedImage inputImage){
+
+        BufferedImage outputImage;
+        outputImage = edgeDetection(inputImage);
+        outputImage = inversion(outputImage);
+
         return outputImage;
     }
 
@@ -170,11 +314,11 @@ public class imageEditor{
         while(iterate){
 
    
-           System.out.println("chose the operation you would like to perform on the image:\n\n1.convert to grey-scale\n2.change brightness\n3.rotate clockwise\n4.rotate anti-clockwise\n5.mirror\n6.blurr.\n7.exit\n");
+           System.out.println("chose the operation you would like to perform on the image:\n\n1.convert to grey-scale\n2.change brightness\n3.rotate clockwise\n4.rotate anti-clockwise\n5.mirror\n6.blur\n7.contrast\n8.crop to circle\n9.edge detection\n10.invert image\n11.pencil sketch\n12.exit\n");
            System.out.print("enter choice: ");
            int choice = sc.nextInt();
 
-           if(choice==7){
+           if(choice==12){
             break;
            }
 
@@ -224,13 +368,46 @@ public class imageEditor{
 
                     case 6: System.out.print("enter length of pixel square: ");
                             int sideLength = sc.nextInt();
-                            //sc.nextLine();
                             BufferedImage blurred = blurr(inputImage , sideLength);
                             File blurredImage = new File("blurred.jpeg");
                             ImageIO.write(blurred , "jpeg" , blurredImage);
                             System.out.println("\ndone!\n");
                             break;
-   
+
+                    case 7: System.out.print("\ncontrast by what percentage: ");
+                            int percentage = sc.nextInt();
+                            BufferedImage contrasted = contrast(inputImage , percentage);
+                            File contrastedImage = new File("contrasted.jpeg");
+                            ImageIO.write(contrasted , "jpeg" , contrastedImage);
+                            System.out.println("\ndone!\n");
+                            break;
+
+                    case 8: System.out.print("\nenter radius: ");
+                            int radius = sc.nextInt();
+                            BufferedImage croppedToCircle = cropToCircle(inputImage , radius);
+                            File croppedToCircleImage = new File("croppedToCircle.jpeg");
+                            ImageIO.write(croppedToCircle , "jpeg" , croppedToCircleImage);
+                            System.out.println("\ndone!\n");
+                            break;
+
+                    case 9: BufferedImage edgeDetected = edgeDetection(inputImage);
+                            File edgeDetectedImage = new File("edgeDetected.jpeg");
+                            ImageIO.write(edgeDetected , "jpeg" , edgeDetectedImage);
+                            System.out.println("\ndone!\n");
+                            break;
+
+                    case 10: BufferedImage inverted = inversion(inputImage);
+                             File invertedImage = new File("inverted.jpeg");
+                             ImageIO.write(inverted , "jpeg" , invertedImage);
+                             System.out.println("\ndone!\n");
+                             break;
+
+                    case 11: BufferedImage pencilSketched = pencilSketch(inputImage);
+                             File pencilSketchedImage = new File("pencilSketched.jpeg");
+                             ImageIO.write(pencilSketched , "jpeg" , pencilSketchedImage);
+                             System.out.println("\ndone!\n");
+                             break;
+
                    default: System.out.println("\nplease enter a valid option.\n");
                }
    
